18397aa02020ccf9d61e67d60120738254c8c6c6
jwhowes <howesjacob8@gmail.com>
2024-12-20 13:51:38 +0000

    implemented a cross attention unet with film

--- src/model/conv.py
+++ src/model/conv.py
@@ -2,7 +2,7 @@ import torch
 
 from torch import nn
 
-from .util import LayerNorm2d
+from .util import LayerNorm2d, FiLM2d, CrossAttention2d, SinusoidalPosEmb
 
 
 class Block(nn.Module):
@@ -22,6 +22,135 @@ class Block(nn.Module):
 
     def forward(self, x):
         return x + self.module(x)
+
+
+class FiLMBlock(nn.Module):
+    def __init__(self, d_model, d_t, hidden_size=None, dropout=0.0, norm_eps=1e-6):
+        super(FiLMBlock, self).__init__()
+        if hidden_size is None:
+            hidden_size = 4 * d_model
+
+        self.dwconv = nn.Conv2d(d_model, d_model, kernel_size=7, padding=3, groups=d_model)
+        self.norm = FiLM2d(d_model, d_t, eps=norm_eps)
+
+        self.ffn = nn.Sequential(
+            nn.Conv2d(d_model, hidden_size, kernel_size=1),
+            nn.GELU(),
+            nn.Conv2d(hidden_size, d_model, kernel_size=1),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x, t):
+        return x + self.ffn(
+            self.norm(
+                self.dwconv(x), t
+            )
+        )
+
+
+class CrossAttentionFiLMBlock(nn.Module):
+    def __init__(self, d_model, d_cond, n_heads, d_t, hidden_size=None, dropout=0.0, norm_eps=1e-6):
+        super(CrossAttentionFiLMBlock, self).__init__()
+        self.attn = CrossAttention2d(d_model, d_cond, n_heads)
+        self.attn_norm = FiLM2d(d_model, d_t, eps=norm_eps)
+
+        self.block = FiLMBlock(d_model, d_t, hidden_size, dropout, norm_eps)
+
+    def forward(self, x, t, cond, attention_mask=None):
+        x = x + self.attn(
+            self.attn_norm(x, t), cond, attention_mask
+        )
+
+        return self.block(x, t)
+
+
+class CrossAttentionFiLMUNet(nn.Module):
+    def __init__(
+            self, in_channels, out_channels=None, d_cond=512, d_t=512, n_heads=(2, 2, 2, 4, 8),
+            dims=(32, 64, 128, 256, 512), depths=(1, 1, 1, 1, 2),
+            dropout=0.0
+    ):
+        super(CrossAttentionFiLMUNet, self).__init__()
+        if out_channels is None:
+            out_channels = in_channels
+
+        self.t_model = nn.Sequential(
+            SinusoidalPosEmb(d_t),
+            nn.Linear(d_t, 4 * d_t),
+            nn.GELU(),
+            nn.Linear(4 * d_t, d_t)
+        )
+
+        self.stem = nn.Conv2d(in_channels, dims[0], kernel_size=7, padding=3)
+
+        self.down_path = nn.ModuleList()
+        self.down_samples = nn.ModuleList()
+        for i in range(len(dims) - 1):
+            down_blocks = nn.ModuleList([
+                CrossAttentionFiLMBlock(dims[i], d_cond, n_heads[i], d_t, dropout=dropout)
+            ])
+            for j in range(depths[i] - 1):
+                down_blocks.append(FiLMBlock(
+                    dims[i], d_t, dropout=dropout
+                ))
+            self.down_path.append(down_blocks)
+            self.down_samples.append(nn.Conv2d(dims[i], dims[i + 1], kernel_size=2, stride=2))
+
+        self.mid_blocks = nn.ModuleList([
+            CrossAttentionFiLMBlock(dims[-1], d_cond, n_heads[-1], d_t, dropout=dropout)
+        ])
+        for i in range(depths[-1] - 1):
+            self.mid_blocks.append(FiLMBlock(dims[-1], d_t, dropout=dropout))
+
+        self.up_samples = nn.ModuleList()
+        self.up_combines = nn.ModuleList()
+        self.up_path = nn.ModuleList()
+        for i in range(len(dims) - 2, -1, -1):
+            self.up_samples.append(nn.ConvTranspose2d(
+                dims[i + 1], dims[i], kernel_size=2, stride=2
+            ))
+            self.up_combines.append(nn.Conv2d(
+                2 * dims[i], dims[i], kernel_size=1
+            ))
+
+            up_blocks = nn.ModuleList([
+                CrossAttentionFiLMBlock(dims[i], d_cond, n_heads[i], d_t, dropout=dropout)
+            ])
+            for j in range(depths[i] - 1):
+                up_blocks.append(FiLMBlock(
+                    dims[i], d_t, dropout=dropout
+                ))
+            self.up_path.append(up_blocks)
+
+        self.head = nn.Conv2d(dims[0], out_channels, kernel_size=7, padding=3)
+
+    def forward(self, x, t, cond, attention_mask=None):
+        x = self.stem(x)
+
+        acts = []
+        for down_blocks, down_sample in zip(self.down_path, self.down_samples):
+            x = down_blocks[0](x, t, cond, attention_mask)
+            for block in down_blocks[1:]:
+                x = block(x, t)
+
+            acts.append(x)
+            x = down_sample(x)
+
+        x = self.mid_blocks[0](x, t, cond, attention_mask)
+        for block in self.mid_blocks[1:]:
+            x = block(x, t)
+
+        for up_blocks, up_sample, up_combine, act in zip(self.up_path, self.up_samples, self.up_combines, acts[::-1]):
+            x = up_combine(torch.concatenate((
+                up_sample(x),
+                act
+            ), dim=1))
+
+            x = up_blocks[0](x, t, cond, attention_mask)
+            for block in up_blocks[1:]:
+                x = block(x, t)
+
+        return self.head(x)
 
 
 class UNet(nn.Module):
--- src/model/util.py
+++ src/model/util.py
@@ -1,6 +1,79 @@
+import torch
+import torch.nn.functional as F
+
 from torch import nn
+from einops import rearrange
+from math import sqrt
 
 
 class LayerNorm2d(nn.LayerNorm):
     def forward(self, x):
         return super().forward(x.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
+
+
+class FiLM2d(nn.Module):
+    def __init__(self, d_model, d_t, *norm_args, **norm_kwargs):
+        super(FiLM2d, self).__init__()
+        self.norm = LayerNorm2d(d_model, *norm_args, elementwise_affine=False, **norm_kwargs)
+        self.gamma = nn.Linear(d_t, d_model)
+        self.beta = nn.Linear(d_t, d_model)
+
+    def forward(self, x, t):
+        B = x.shape[0]
+
+        return self.gamma(t).view(B, -1, 1, 1) * self.norm(x) + self.beta(t).view(B, -1, 1, 1)
+
+
+class CrossAttention2d(nn.Module):
+    def __init__(self, d_model, d_cond, n_heads):
+        super(CrossAttention2d, self).__init__()
+        assert d_model % n_heads == 0
+        self.n_heads = n_heads
+        self.scale = sqrt(d_model // n_heads)
+
+        self.W_q = nn.Linear(d_model, d_model, bias=False)
+        self.W_k = nn.Linear(d_cond, d_model, bias=False)
+        self.W_v = nn.Linear(d_model, d_model, bias=False)
+
+        self.W_o = nn.Conv2d(d_model, d_model, kernel_size=1)
+
+    def forward(self, image, cond, attention_mask=None):
+        L_cond = cond.shape[1]
+        B, _, H, W = image.shape
+
+        q = rearrange(
+            self.W_q(rearrange(
+                image, "b d h w -> b (h w) d"
+            )), "b l (n d) -> b n l d", n=self.n_heads
+        )
+        k = rearrange(self.W_k(cond), "b l (n d) -> b n l d", n=self.n_heads)
+        v = rearrange(self.W_v(cond), "b l (n d) -> b n l d", n=self.n_heads)
+
+        attn = (q @ k.transpose(-2, -1)) / self.scale
+
+        if attention_mask is not None:
+            attn = attn + attention_mask.view(B, 1, -1, L_cond)
+
+        return self.W_o(
+            rearrange(F.softmax(attn, dim=-1) @ v, "b n (h w) d -> b (n d) h w", h=H, w=W)
+        )
+
+
+class SinusoidalPosEmb(nn.Module):
+    def __init__(self, d_model, base=1e5):
+        super(SinusoidalPosEmb, self).__init__()
+        assert d_model % 2 == 0
+        self.register_buffer(
+            "theta",
+            1.0 / (base ** (2 * torch.arange(d_model // 2) / d_model)),
+            persistent=False
+        )
+
+    def forward(self, x):
+        B = x.shape[0]
+        x = x.float().view(-1, 1) * self.theta
+
+        return torch.stack((
+            x.cos(),
+            x.sin()
+        ), dim=-1).view(B, -1)
